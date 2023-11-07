611e5bbd633578b07c6093eb698785cd1427bcf0
liucharlie213 <liu.charlie213@gmail.com>
2023-11-06 22:34:57 -0500

    all phone dimensions added

--- roryslivekaroake/src/components/carousel/Carousel.js
+++ roryslivekaroake/src/components/carousel/Carousel.js
@@ -40,26 +40,30 @@ return (
     <p className="carousel__quote">PICK A FAN FAVOURITE...</p>
     {/* <p className="carousel__title"><span className="carousel__span">Popular Songs</span></p> */}
     <div className="carousel">
-      <button onClick={previousSong} className="carousel__buttons">
-        <img src={ prevArrow } /> 
-      </button>
+      
       <div className="song__info">
         {songs.length > 0 ? ( // Add this check
           <>
-            <img src={songs[currentIndex].Cover} id="song__cover"/>
+          <img src={songs[currentIndex].Cover} id="song__cover" alt=""/>
+          <div className="browse__popular">  
+            <button onClick={previousSong} className="carousel__buttons">
+              <img src={ prevArrow } alt=""/> 
+            </button>
             <div className="song__text">
               <p id="song__title">{songs[currentIndex].Title}</p>
               <p id="song__artist">{songs[currentIndex].Artist}</p>
             </div>
+            <button onClick={nextSong} className="carousel__buttons">
+              <img src={ nextArrow } alt=""/> 
+            </button>
             
+          </div>
           </>
         ) : (
           <p>Loading...</p> // Show a loading message or some placeholder
         )}
       </div>
-      <button onClick={nextSong} className="carousel__buttons">
-        <img src={ nextArrow } /> 
-      </button>
+      
     </div>
     
   </div>
--- roryslivekaroake/src/components/gallery/Gallery.js
+++ roryslivekaroake/src/components/gallery/Gallery.js
@@ -1,20 +1,49 @@
-import React from "react"
+import React, { useState} from "react"
 import image1 from "../../assets/photos/RorysLK image1.png"
 import image2 from "../../assets/photos/RorysLK image2.png"
 import image3 from "../../assets/photos/RorysLK image3.png"
+import prevArrow from "../../assets/photos/prev arrow.png"
+import nextArrow from "../../assets/photos/next arrow.png"
 import "./gallery.css"
 
 const Gallery = () => {
+
+  const photos = [image1, image2, image3];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextPhoto = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % photos.length);
+  };
+
+  const previousPhoto = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + photos.length) % photos.length);
+  };
+
   return (
     <div className="gallery">
         {/* <p className="gallery__title">Gallery</p> */}
-        <span className="gallery__background">
+        <div className="computer__gallery">
           <div className="gallery__photos">
-            <img src={image1} className="gallery__photo"></img>
-            <img src={image2} className="gallery__photo"></img>
-            <img src={image3} className="gallery__photo"></img> 
+            <img src={image1} className="gallery__photo" alt=""></img>
+            <img src={image2} className="gallery__photo" alt=""></img>
+            <img src={image3} className="gallery__photo" alt=""></img> 
           </div>
-        </span>
+        </div>
+
+        <div className="phone__gallery">
+          <div className="gallery__photo">
+            <img src={photos[currentIndex]} alt="" id="phone__image"></img>
+          </div>
+          <div className="gallery__buttons">
+              <button onClick={previousPhoto} className="gallery__button">
+                <img src={ prevArrow } alt="" /> 
+              </button>
+              <button onClick={nextPhoto} className="gallery__button">
+                <img src={ nextArrow } alt=""/> 
+              </button>
+            </div>
+        </div>
+        
       
 
     </div>
